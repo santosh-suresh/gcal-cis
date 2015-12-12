d6a6f300a70f938cb6c01a356e0baac8b34ddef6
Santosh Suresh <santoshdeshpande@gmail.com>
2015-12-12 08:43:06 +0530

    Initial version of the CIS app for GCal

---
+++ src/main/java/com/jifflenow/cis/GoogleCalCisApplication.java
@@ -0,0 +1,13 @@
+package com.jifflenow.cis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCalCisApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Getting ready");
+        SpringApplication.run(GoogleCalCisApplication.class, args);
+    }
+}
